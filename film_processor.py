12ee12d3d696244895ccedb4b08ffde34ed88ffa
rjandersson1 <rjandersson.1@gmail.com>
2025-06-29 13:53:19 -0700

    implemented renderer class. initial testing.

--- film_processor.py
+++ film_processor.py
@@ -14,18 +14,36 @@ sys.path.append(os.path.abspath(r'C:\A_Documents\Documents\Coding\Lightroom_File
 # Import and reload modules
 import RollMetadataDef
 import FilmCollectionDef
+import Renderer
 importlib.reload(RollMetadataDef)  # Force reload of RollMetadataDef
 importlib.reload(FilmCollectionDef)  # Force reload of FilmCollectionDef
+importlib.reload(Renderer)  # Force reload of Renderer
 
 # Import the classes from the reloaded modules1
 from RollMetadataDef import RollMetadata
 from FilmCollectionDef import FilmCollection
+from Renderer import Renderer
+
 
 # Define the file path
 # fileDir = r'C:\A_Documents\Documents\Coding\Lightroom_FileFinder\testFiles\Film Scanning'
-fileDir = r'C:\A_Documents\Photography\Film Scanning'
+# fileDir = r'C:\A_Documents\Photography\Film Scanning'
+# fileDir = r'/Users/rja/Photography/Film Scanning'
+fileDir = r'/Users/rja/Documents/Coding/film-photo-archive-manager/data/filmCollectionTest'
 # fileDir = r'D:\backup film scanning 26.06.23'
 
 collection = FilmCollection(fileDir)
 
-collection.plot.cumulative_photos_over_time_with_span_and_labels('date','cameraModel','2021.01.01','2024.01.01')
+for i in range(5):
+    print('\n')
+
+rollA = collection.getRoll(74)
+# print(rollA.rollName)
+# print(rollA.get_info_image())
+# collection.help()
+# print(rollA.camera)
+# print(rollA.image_data[1])
+# print(rollA.title)
+
+renderer = Renderer(rollA)
+renderer.run()
