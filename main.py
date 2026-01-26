e1e97a220b8cad6ca3504f1d455a2c5c45e2920d
rjandersson1 <rjandersson.1@gmail.com>
2026-01-26 09:53:40 +0100

    testing exif importing

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-#TODO: fix the shit with duplicates on roll 35 ???
-    # changed all duplicated indeces to '#9XX' to try to fix it but it now half works? still finding index duplicate issues
-    # also bug in copies: its passing the copy to the correct master, but the index of the copy is totally wrong to the master.
-    # hardcode a solution for [083][05] location unknown error
-
-# final check
-# 0-10 good, [8] has STD folder conflicts --> rename to 5mb. [8] also has location error on two images
-# 10-20 good
-# 20-30 good, had to rework copy finding on [25] but works now
-# 30-40 needs work:
-#           [35] bologna works now, but only bc I have changed #99 for all VCs. 32 as misidentified 4 as master to 3. Reworked copy finding. [035][34] and [035][35] have location issues (fixed).
-#           [33] and [36] had camera exif issues on some images (model='SONY'), (make='DSLR-A550') --> hardcoded a fix in verify_camera in exposureObj
-# 40-50 good, [45][46] both don't have states given (sweden/denmark). Added clause for if location but !state, then ignore location err.
-#             [41] had camera exif issues on some images (model='SONY'), (make='DSLR-A550') --> hardcoded a fix in verify_camera in exposureObj  
-# 50-60 good
-# 60-70 good, [65] and [66] missing jpg but expected. [67][7] wrong master/copy rel. [68] [02]&[03] have identical date, so copy is checked. Hardcode to avoid checking copies on this roll
-# 70-80 good, had to remove jpgDirs when 5mb/5mp was in jpgdirs, fixed double rawDir path setting. [78] has camera cast error (fixed), and raw missing for [78][15] (fixed): [78][15] had tif panorama quirks
-# 80-90 good, [84] was never exported? no jpegs?? need to reexport...
-# 90-92 good
 
 # TODO:
 # reexport [84]
 # handle HDR
 # refactor functions and clean everything up...
 
+# reworking exif saving: (tests done for 072, 074, 083, 085)
+# - v0: cmd request fetch exif every image (batched by roll, with +10s savings).
+#               benchmark time:     65.00s (full! old library)
+#               time to clean rolls:     80.00s 
+#               benchmark time:     3.08s (old library)
+#               benchmark time:     3.08s (cleaned library)
+# - v1: export jsons
+#              time to clean rolls:     70.00s
+# - v2: import from existing exif
+#              time to clean rolls:     81.00s
+#              benchmark time:      2.51 (cleaned, json exif import)
+
+
+# 1-10
+# 11-20
+# 21-30
+# 31-40 124s        [40] has two cameras on the roll!
+# 41-50
+# 51-60
+# 61-70
+# 71-80
+# 81-90
+# 91-100
 
 import numpy as np
 import pandas as pd
@@ -49,19 +53,21 @@ importlib.reload(renderTool)
 importlib.reload(importTool)
 
 # ======================== Setup Vars ================================
-DEVMODE = 0         # If true, work in local dir. If false, work in production dir. Contains rolls 72, 74, 83, 85
+DEVMODE = 1         # If true, work in local dir. If false, work in production dir. Contains rolls 72, 74, 83, 85
+CLEANMODE = 1    # import from cleaned library
+EXTERNAL_SSD = 0
 
-# rolls_to_import = 'all'
+rolls_to_import = 'all'
 # rolls_to_import = [72, 74, 83, 85]
-# rolls_to_import = '91-95'
-rolls_to_import = [9]
+# rolls_to_import = '31-40'
+# rolls_to_import = [72]
 
 
 # deine wallpaper path
 wallpaper_path = r'/Users/rja/Documents/Wallpapers/wallpapers-1'
 
 # define library path for cleaning up
-cleanup_path = r'/Users/rja/Documents/Coding/film-photo-archive-manager/data/photography/film/library'
+library_clean = r'/Users/rja/Documents/Coding/film-photo-archive-manager/data/photography'
 
 
 # ====================================================================
@@ -72,9 +78,14 @@ if DEVMODE:
     # Set abspath and reference library to this workspace
     sys.path.append(os.path.abspath(r'C:\A_Documents\Documents\Coding\Lightroom_FileFinder'))
     library = r'/Users/rja/Documents/Coding/film-photo-archive-manager/data/filmCollectionTestAll'
+    if CLEANMODE:
+        library = r'/Users/rja/Documents/Coding/film-photo-archive-manager/data/photography/film/library'
 else:
     sys.path.append(os.path.abspath(r'C:\A_Documents\Documents\Coding\Lightroom_FileFinder'))
     library = r'/Users/rja/Photography/Film Scanning'
+    if EXTERNAL_SSD:
+        library = r'/Volumes/NVME_B/backup_NVME_A_08.05.2025/A_Documents/Photography/Film Scanning'
+        library_clean = r'/Volumes/NVME_B/photography'
 
 print("===================================================================")
 print("===================================================================")
@@ -114,28 +125,23 @@ for i in range(3):
 # =================================================================================
 
 
-display_preview = 1
-display_preview = 0
-display_info = 0
-display_info = 1
-
-preview_size = 75
-for roll in collection.rolls:
-    for img in roll.images:
-        if img.containsCopies and display_info:
-            print(f'[{img.roll.index_str}][{img.index_str}]\t{img.copyCount}x')
-            print(f'\t\tMASTER\t{img.index_original}\t{img.copyType}\t{img.rawFileName}\t{img.fileSize/1024/1024:.0f}Mb\t{img.mpx:.0f}MP\t{img.aspectRatio:.2f}:1\tisColor:{int(img.isColor)}\tisBlackAndWhite:{int(img.isBlackAndWhite)}\tisGrayscale:{int(img.isGrayscale)}')
-            if display_preview: img.display(preview_size)
-            for copy in img.copies:
-                if display_preview: copy.display(preview_size)
-                print(f'\t\tCOPY\t{copy.index_original}\t{copy.copyType}\t{copy.rawFileName}\t{copy.fileSize/1024/1024:.0f}Mb\t{copy.mpx:.0f}MP\t{copy.aspectRatio:.2f}:1\tisColor:{int(copy.isColor)}\tisBlackAndWhite:{int(copy.isBlackAndWhite)}\tisGrayscale:{int(copy.isGrayscale)}')
-            print('\n')
 
 
-# importer.generate_wallpapers_bw(collection.rolls, wallpaper_path, rating_limit=3, size_limit=1)
+# for roll in collection.rolls:
+#     importer.cleanRoll(roll, library_path=library_clean, mode=[1,1,1,1,0,1])
 
-for img in collection.rolls[0].images_all:
-    print(img.index, img.location, img.state, img.country)
+# TODO: bug when exporting a roll, raw missing?
+
+# for roll in collection.rolls:
+#     print('Cameras on roll:', roll.cameras)
+#     for img in roll.images_all:
+#         print(img.index, img.camera, '  -->  ', img.cam)
+
+
+
+
+
+
 
 
 # =================================================================================
@@ -143,3 +149,23 @@ for i in range(3):
     print('\n')
 print("===================================================================")
 db.i('[I]', f"Runtime: {time() - runtime_t0:.2f}s")
+
+# display_preview = 1
+# display_preview = 0
+# display_info = 0
+# display_info = 1
+
+# preview_size = 75
+# for roll in collection.rolls:
+#     for img in roll.images:
+#         if img.containsCopies and display_info:
+#             print(f'[{img.roll.index_str}][{img.index_str}]\t{img.copyCount}x')
+#             print(f'\t\tMASTER\t{img.index_original}\t{img.copyType}\t{img.rawFileName}\t{img.fileSize/1024/1024:.0f}Mb\t{img.mpx:.0f}MP\t{img.aspectRatio:.2f}:1\tisColor:{int(img.isColor)}\tisBlackAndWhite:{int(img.isBlackAndWhite)}\tisGrayscale:{int(img.isGrayscale)}')
+#             if display_preview: img.display(preview_size)
+#             for copy in img.copies:
+#                 if display_preview: copy.display(preview_size)
+#                 print(f'\t\tCOPY\t{copy.index_original}\t{copy.copyType}\t{copy.rawFileName}\t{copy.fileSize/1024/1024:.0f}Mb\t{copy.mpx:.0f}MP\t{copy.aspectRatio:.2f}:1\tisColor:{int(copy.isColor)}\tisBlackAndWhite:{int(copy.isBlackAndWhite)}\tisGrayscale:{int(copy.isGrayscale)}')
+#             print('\n')
+
+
+# importer.generate_wallpapers_bw(collection.rolls, wallpaper_path, rating_limit=3, size_limit=1)
